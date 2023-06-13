615e8aca091fc126d4c5a3fc612ac633687f21ed
orrho <orr.horev@gmail.com>
2023-06-13 15:18:28 +0300

    orrho new

--- PorojectPart2/static/script.js
+++ PorojectPart2/static/script.js
@@ -44,4 +44,66 @@ function calculateGrade() {
     var resultElement = document.getElementById("result");
     resultElement.innerHTML = "Your grade average is: " + average.toFixed(2);
   }
+
+
+  /* page 2 */
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Get the grades from Page 1
+    const grades = {
+      hadwah: parseInt(localStorage.getItem('grade1')),
+      physics: parseInt(localStorage.getItem('grade2')),
+      maam: parseInt(localStorage.getItem('grade3')),
+      aviro: parseInt(localStorage.getItem('grade4')),
+      meta: parseInt(localStorage.getItem('grade5')),
+      platform: parseInt(localStorage.getItem('grade6'))
+    };
+  
+    // Define the easiness levels and points
+    const easiness = {
+      hadwah: 2,
+      physics: 2.5,
+      maam: 3.5,
+      aviro: 2.5,
+      meta: 5.5,
+      platform: 5
+    };
+  
+    const points = {
+      hadwah: 6,
+      physics: 5.5,
+      maam: 2,
+      aviro: 5,
+      meta: 2.5,
+      platform: 3
+    };
+  
+    const scores = {};
+  
+    // Calculate the score for each subject
+    for (const subject in grades) {
+      const grade = grades[subject];
+      const easinessValue = easiness[subject];
+      const point = points[subject];
+      const score = 90 * easinessValue - grade * point;
+      scores[subject] = score;
+    }
+  
+    let bestSubject = null;
+    let maxScore = -Infinity;
+  
+    // Find the subject with the highest score
+    for (const subject in scores) {
+      if (scores[subject] > maxScore) {
+        maxScore = scores[subject];
+        bestSubject = subject;
+      }
+    }
+  
+    const bestSubjectElement = document.getElementById('bestSubject');
+    const scoreElement = document.getElementById('score');
+  
+    bestSubjectElement.textContent = "Best subject to improve: " + bestSubject;
+    scoreElement.textContent = "Score for the best subject: " + maxScore;
+  });
   
